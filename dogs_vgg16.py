e026b362da6c675d7ef77f6a09069e512c16d412
Ubuntu <ubuntu@ip-172-31-14-119.us-west-2.compute.internal>
2017-12-20 20:24:01 +0000

    experiments with vgg16)

--- dogs_vgg16.py
+++ dogs_vgg16.py
@@ -1,12 +1,26 @@
+import numpy as np
+import pandas as pd
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Dropout, Input, Activation
+from keras.layers import Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras.optimizers import Adam
+from keras.applications import VGG16
+from keras.applications import ResNet50
+from keras import Model
+from keras.preprocessing import image
+import cv2
+from sklearn.model_selection import train_test_split
 def prepare_train_validation():
+    image_size = 128
+    labels = pd.read_csv('data/labels.csv.zip',compression='zip')
     #converting breed categories to categorical booleans
     one_hot_labels = pd.get_dummies(labels['breed']).values
     X_raw = []
     y_raw = []
+
     #loads and processes the images into image_size by image_size by 3 tensors
     for ind, row in enumerate(labels.values):
-        labels = pd.read_csv('../data/dogs/data/labels.csv.zip',compression='zip')
-        img = cv2.imread('../data/dogs/data/train/{}.jpg'.format(row[0]))
+        img = cv2.imread('data/train/{}.jpg'.format(row[0]))
         img = cv2.resize(img,(image_size,image_size))
         X_raw.append(img)
         y_raw.append(one_hot_labels[ind,:])
@@ -16,7 +30,7 @@ def prepare_train_validation():
 
     return X_train, X_validation, y_train, y_validation
 X_train, X_validation, y_train, y_validation = prepare_train_validation()
-
+image_size = 128
 vgg_16_conv = VGG16(weights='imagenet',include_top = False)
 img_in = Input(shape = (image_size,image_size,3),name = 'image_input')
 outputVGG16 = vgg_16_conv(img_in)
@@ -25,6 +39,7 @@ X = Dense(2048, activation='relu',name="dense1")(X)
 X = Dense(2048, activation='relu',name="dense2")(X)
 X = Dense(120, activation='softmax', name="output")(X)
 adapted_VGG16 = Model(input = img_in,output = X)
-adapted_VGG16.compile(optimizer = 'adam', loss = 'categorical_crossentropy',metrics = ['accuracy'] )
+Ad = Adam(lr = .01 , beta_1 = .9, beta_2 = .999, epsilon = 1e-08, decay =0.0)
+adapted_VGG16.compile(optimizer =Ad, loss = 'sparse_categorical_crossentropy',metrics = ['accuracy'] )
 
 adapted_VGG16.fit(X_train,y_train,epochs = 10, batch_size = 19)
